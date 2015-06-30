0b2b50b8715f8de25ff349006600cf968c60f68c
Rudolph Bott <r@bott.im>
2015-06-30 19:38:15 +0200

    added support for pushover api

---
+++ notify.php
@@ -0,0 +1,83 @@
+<?php
+
+function sendPushMessage($pushMessage,$apiUrl,$apiToken,$apiUser) {
+	curl_setopt_array($ch = curl_init(), array(
+		CURLOPT_URL => $apiUrl,
+		CURLOPT_POSTFIELDS => array(
+			"token" => $apiToken,
+			"user" => $apiUser,
+			"message" => $pushMessage,
+		),
+		CURLOPT_SAFE_UPLOAD => true,
+		CURLOPT_RETURNTRANSFER => true,
+	));
+	curl_exec($ch);
+	curl_close($ch);
+}
+
+# basic variables
+include("pushover-config.php");
+$lastAvgValueFile = "/tmp/average-temp.txt";
+
+# homegear stuff
+include_once('includes/homematic.php');
+$home = new HomeMaticInstance;
+$devices = $home->getAllDevices(true);
+
+$temperatures = array();
+foreach($devices AS $device) {
+	if(!empty($device["tempSensor"])) {
+		$temperatures[] = $device["tempSensor"];
+	}
+}
+
+if(count($temperatures) > 0) {
+	$avgTemp = round(array_sum($temperatures) / count($temperatures),2);
+}
+else {
+	$avgTemp = 0.0;
+}
+
+# read last value
+if(file_exists($lastAvgValueFile)) {
+	$fp = fopen($lastAvgValueFile,"r");
+	if($fp) {
+		$line = fgets($fp,1024);
+		$lastAvgValue = round(floatval($line),2);
+		if(!is_float($lastAvgValue)) {
+			$lastAvgValue = 0.0;
+		}
+		fclose($fp);
+	}
+}
+else {
+	$lastAvgValue = 0.0;
+}
+
+# store new value
+$fp = fopen($lastAvgValueFile,"w");
+if($fp) {
+	fputs($fp,$avgTemp);
+	fclose($fp);
+}
+
+# generate Notifications
+if($lastAvgValue > $avgTemp) {
+	if($lastAvgValue - $avgTemp > 10) {
+		$pushMessage = "AVG temp dropped by more than 10°C! (" . $lastAvgValue . " -> " . $avgTemp . ")";
+		sendPushMessage($pushMessage,$apiUrl,$apiToken,$apiUser);
+	}
+}
+else {
+	if($avgTemp - $lastAvgValue > 10) {
+		$pushMessage = "AVG temp raised by more than 10°C! (" . $lastAvgValue . " -> " . $avgTemp . ")";
+		sendPushMessage($pushMessage,$apiUrl,$apiToken,$apiUser);
+	}
+}
+if($avgTemp >= 28 && $lastAvgValue < 28) {
+	$pushMessage = "AVG temp now OVER 28°C! (" . $avgTemp . ")";
+	sendPushMessage($pushMessage,$apiUrl,$apiToken,$apiUser);
+}
+
+
+?>
--- test.php
+++ test.php
@@ -4,26 +4,26 @@ include_once('includes/homematic.php');
 include_once('includes/temperatures.php');
 $home = new HomeMaticInstance;
 
-echo "Available Valves:\n";
-print_r($home->getValveNames());
-
-echo "Selecting 'Heizung-Kueche'\n";
-$kueche = $home->getValveByName("Heizung-Kueche");
-
-#echo "Setting Target Temperature to 20.0°C\n";
-#$kueche->setTargetTemp(20.0);
-
-echo "Printing all Temperature Sensors:\n";
-print_r($home->getAllTemperatures());
-
-echo "Printing all Humidity Sensors:\n";
-print_r($home->getAllHumidity());
-
-echo "Printing all Control Mode Settings in Valves:\n";
-print_r($home->getAllControlModes());
-
-echo "Printing all Temperature Fall Mode Settings in Valves:\n";
-print_r($home->getAllTempFallModes());
+#echo "Available Valves:\n";
+#print_r($home->getValveNames());
+#
+#echo "Selecting 'Heizung-Kueche'\n";
+#$kueche = $home->getValveByName("Heizung-Kueche");
+#
+##echo "Setting Target Temperature to 20.0°C\n";
+##$kueche->setTargetTemp(20.0);
+#
+#echo "Printing all Temperature Sensors:\n";
+#print_r($home->getAllTemperatures());
+#
+#echo "Printing all Humidity Sensors:\n";
+#print_r($home->getAllHumidity());
+#
+#echo "Printing all Control Mode Settings in Valves:\n";
+#print_r($home->getAllControlModes());
+#
+#echo "Printing all Temperature Fall Mode Settings in Valves:\n";
+#print_r($home->getAllTempFallModes());
 
 #echo "Setting Control Mode to 'auto' on all valves:\n";
 #$home->setAllControlModes('manual');
