cafbd86c652a70a3f6ebf5334a34254dcab610e5
Rudolph Bott <r@bott.im>
2016-01-06 00:05:45 +0100

    move to slim/twig/boostrap

---
+++ public/index.php
@@ -0,0 +1,62 @@
+<?php
+
+# wilfried-kuefen.de
+# Copyright (C) 2015  Rudolph Bott
+#
+
+require_once('../includes/Twig/Autoloader.php');
+require_once('../includes/Slim/Slim.php');
+
+require_once('../config/config.inc.php');
+require_once('../includes/homematic.php');
+
+\Slim\Slim::registerAutoloader();
+
+$app = new \Slim\Slim(array(
+	'templates.path' => '../tpl'
+));
+
+/*
+$app->add(new \Slim\Middleware\HttpBasicAuthentication([
+	"path" => "/admin",
+	"realm" => "Protected",
+	"secure" => false,
+	"users" => [
+		"rudi" => "hgadmin",
+	]
+]));
+*/
+
+$app->view(new \Slim\Views\Twig());
+$app->view->parserExtensions = array(new \Slim\Views\TwigExtension());
+
+$app->get('/', function() use ($app) {
+	Header("Location: /overview");
+	exit;
+});
+
+$app->get('/overview', function() use ($app) {
+	$site = new homeMaticInstance();
+	$devices = $site->getAllDevices(true);
+	$app->render('overview.html', array("devices" => $devices,));
+});
+
+$app->post('/setTemp', function() use ($app) {
+	$site = new homeMaticInstance();
+	if($valve = $site->getValveByPeerId($_POST["peerId"])) {
+		$valve->setTargetTemp(floatval($_POST["targetTemp"]));
+	}
+	Header("Location: /overview");
+	exit;
+});
+
+$app->post('/setAllTemp', function() use ($app) {
+	$site = new homeMaticInstance();
+	$site->setTargetTemperature(floatval($_POST["targetTemp"]));
+	Header("Location: /overview");
+	exit;
+});
+
+$app->run();
+
+?>
