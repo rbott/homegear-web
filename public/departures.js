44558ef4d90d61bec81f179f04eef7d9882b02f6
Rudolph Bott <r@spam.wtf>
2022-02-05 18:16:33 +0100

    get live departures data

--- public/departures.js
+++ public/departures.js
@@ -1,17 +1,12 @@
 function getDeparturesData() {
-    // fetch-Aufruf mit Pfad zur XML-Datei
-    fetch('https://openservice-test.vrr.de/static02/XML_DM_REQUEST?sessionID=0&requestID=0&language=DE&type_dm=stopID&name_dm=20020105')
-        .then(function(response) {
-            return response.text();
-        })
-        .then(function(data) {
-            console.log(data);
+	const myRequest = new Request('/transportation');
 
-            let parser = new DOMParser(),
-                xmlDoc = parser.parseFromString(data, 'text/xml');
-
-
-        }).catch(function(error) {
-            console.log("Fehler: bei Auslesen der XML-Datei " + error);
-        });
+	fetch(myRequest)
+		.then(response => response.json())
+		.then(data => {
+			for (const element of data.elements) {
+				console.log(element)
+			}
+		})
+		.catch(console.error);
 }
