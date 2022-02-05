42183131d7672c9c5e436ef44c2c8e29235d1a71
Rudolph Bott <r@spam.wtf>
2022-02-05 18:56:39 +0100

    fix method name

--- public/index.php
+++ public/index.php
@@ -135,7 +135,7 @@ $app->get('/heaters', function() use ($app) {
 	foreach($devices as $device) {
 		$return_data[] = [
 			"name" => $device->getName(),
-			"temperature" => (float)$device->getTemperature(),
+			"temperature" => (float)$device->getTempSensor(),
 			"valve" => (int)$device->getValveState(),
 			"target" => (float)$device->getTargetTemp()
 		];
